2c53616131e8265aac10a06e078f8ff18c899175
allen.w <koomoxs@gmail.com>
2020-04-06 22:30:09 +0800

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+// Import bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Render main app
+ReactDOM.render(<App/>, document.getElementById('app'));
